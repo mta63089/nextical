257ce84601471bef1f9188af65ccd5024f018c86
mta630 <mta630@ddubb.net>
2025-04-01 20:42:29 -0600

    adds hooks and toolbarplugin for shadcn-editor

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,46 @@
-export default function Home() {
-  return <div></div>;
+"use client";
+
+import { Editor } from "@/components/blocks/editor/editor";
+import { SerializedEditorState } from "lexical";
+import { useState } from "react";
+
+export const initialValue = {
+  root: {
+    children: [
+      {
+        children: [
+          {
+            detail: 0,
+            format: 0,
+            mode: "normal",
+            style: "",
+            text: "Hello World 🚀",
+            type: "text",
+            version: 1,
+          },
+        ],
+        direction: "ltr",
+        format: "",
+        indent: 0,
+        type: "paragraph",
+        version: 1,
+      },
+    ],
+    direction: "ltr",
+    format: "",
+    indent: 0,
+    type: "root",
+    version: 1,
+  },
+} as unknown as SerializedEditorState;
+
+export default function EditorPage() {
+  const [editorState, setEditorState] =
+    useState<SerializedEditorState>(initialValue);
+  return (
+    <Editor
+      editorSerializedState={editorState}
+      onSerializedChange={(value) => setEditorState(value)}
+    />
+  );
 }
